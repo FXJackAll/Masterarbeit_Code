e47d62d0a61ead9fc0ea9fe43f5e13be40ffea96
Felix Caesar Madorskiy <mai12bmc@studserv.uni-leipzig.de>
2025-11-26 22:07:46 +0100

    Masterarbeit weiter geschrieben

--- src/main.py
+++ src/main.py
@@ -61,11 +61,11 @@ def user_input():
     # parser.add_argument('-p', '--problem', type=str, default='rat195',
     #                    help='Name of the problem instance, e.g. the TSPLIB names like "rat195"')
 
-    parser.add_argument('-p', '--problem', type=str, default='b_2_2',
+    parser.add_argument('-p', '--problem', type=str, default='csd_3_penalty_penalty',
                         help='Name of the problem instance, e.g. TISD names like "tisd10" or TSPLIB names like "rat195"')
     # parser.add_argument('-pt', '--problem-type', type=str, default='TSP',
     #                    help='Type of the problem, e.g. TSP (standard, symmetric TSP), ATSP (asymmetric TSP), QAP, TISD')
-    parser.add_argument('-pt', '--problem-type', type=str, default='B_2',
+    parser.add_argument('-pt', '--problem-type', type=str, default='CSD',
                         help='Type of the problem, e.g. TSP (standard, symmetric TSP), ATSP (asymmetric TSP), QAP, TISD')
 
     parser.add_argument('-rot', '--rotation', action=argparse.BooleanOptionalAction, default=False,
@@ -88,7 +88,7 @@ def user_input():
                              'absolute: |f - f*| < Term, relative: |f - f*| < |Term * f| + Term')
     parser.add_argument('-abs', '--absolute_accuracy_term', type=float, default=10**(-10),
                         help='Absolute accuracy at which the algorithm;')
-    parser.add_argument('-rel', '--relative_accuracy_term', type=float, default=10**(-4),
+    parser.add_argument('-rel', '--relative_accuracy_term', type=float, default=10**(-6),
                         help='Relative accuracy at which the algorithm;')
 
 
@@ -104,7 +104,7 @@ def user_input():
                         help='The number of fixed restarts the algorithm performs')
     parser.add_argument('-noi', '--number_of_iterations_per_run', type=int, default=8000,
                         help='The number of iterations per run')
-    parser.add_argument('-po', '--problem_optimum', type=float, default=0,
+    parser.add_argument('-po', '--problem_optimum', type=float, default=2.658559,
                         help='the known optimum of the problem')
     # parser.add_argument('-as', '--accepted_solutions', type=str, choices=['exact-term', 'penalty-term'], default='exact-term',
     #                    help='The way initial solutions are calculated; exact-term means only initial solutions that fit the constrains'
